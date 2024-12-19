f05488e3dde6bcce33c6cbb542fe184c0ffaf2e7
kyungho <epsoe@naver.com>
2024-12-19 11:02:57 +0900

    navigation

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,10 @@
 import Navigation from '../components/navigation';
 
-export default function Home() {
+export default function NotFound() {
   return (
     <div>
       <Navigation />
-      <h1>Home</h1>
+      <h1>Not Found</h1>
     </div>
   );
 }
